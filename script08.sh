e30957190fb68feac604ad42dd6ab9b62a9506fe
cj_global <cj_global@qq>
2024-04-19 18:54:18 +0100

    [chao]  first commit

---
+++ script08.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/vol/hpc-software/software/Anaconda3/2020.07/bin/python /user/HS500/cj00677/condor-examples/mnist_pytorch.py
---
+++ script09.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/user/HS500/cj00677/condor-examples/miniconda3/envs/tensorflow/bin/python /user/HS500/cj00677/condor-examples/mnist_deep.py  --num_iterations 5000
